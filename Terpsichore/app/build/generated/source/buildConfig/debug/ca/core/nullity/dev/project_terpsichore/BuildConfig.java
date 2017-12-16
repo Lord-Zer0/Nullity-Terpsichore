8e2d7b8f1f138e69c2dcfbab3097191c0e4187e5
Connor McCauley <cdmc13@outlook.com>
2017-12-16 01:42:31 -0500

    gradle builds

---
+++ Terpsichore/app/build/generated/source/buildConfig/debug/ca/core/nullity/dev/project_terpsichore/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ca.core.nullity.dev.project_terpsichore;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ca.core.nullity.dev.project_terpsichore";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
